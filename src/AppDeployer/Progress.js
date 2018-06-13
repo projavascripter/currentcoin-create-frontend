39f7b876e7dc8d8d06acc465551d625b0ba6afb2
jason00111 <emberleyjason@gmail.com>
2018-06-12 18:38:08 -0700

    updates Progress to allow simultaneous deploy and transfer

--- src/AppDeployer/Progress.js
+++ src/AppDeployer/Progress.js
@@ -4,15 +4,36 @@ const etherscanAddress = 'https://rinkeby.etherscan.io'
 
 /*
   The shape of this.props.tasks is
-  {
-    serviceAddress0: { state, txHash, serviceUrl, errorMessage },
-    serviceAddress1: { state, txHash, serviceUrl, errorMessage },
-    serviceAddress2: { state, txHash, serviceUrl, errorMessage },
-    ...
-  }
-
-  state can be one of 0, 1, 2, 3
-  or one of 'transferring', 'deploying', 'done', 'error'
+    {
+      serviceAddress0: {
+        transferState: {
+          inProgress: bool,
+          complete: bool,
+          error: bool,
+          transactionHash,
+          errorMessage,
+        },
+        deployState: {
+          inProgress: bool,
+          complete: bool,
+          error: bool,
+          serviceUrl,
+          errorMessage,
+        },
+      },
+      serviceAddress1: {
+        transferState: {
+          ...
+        },
+        deployState: {
+          ...
+        },
+      },
+      serviceAddress2: {
+        ...
+      },
+      ...
+    }
 */
 
 class Progress extends Component {
@@ -20,92 +41,164 @@ class Progress extends Component {
     if (Object.keys(this.props.tasks).length === 0)
       return null
 
-    return <div className='progress column'>
-      {
-        Object.keys(this.props.tasks).map(serviceAddress => {
-          const {
-            state,
-            txHash,
-            serviceUrl,
-            errorMessage,
-          } = this.props.tasks[serviceAddress]
+    return (
+      <div className='progress column'>
+        {
+          Object.keys(this.props.tasks).map(serviceAddress => {
+            const {
+              transferState,
+              deployState,
+            } = this.props.tasks[serviceAddress]
 
-          if (state === 'error' || state === 3) {
             return [
               <div className='progress-row' key='0'>
                 {
-                  errorMessage
-                    ? errorMessage
-                    : 'There was a problem deploying the service.'
+                  transferState.error
+                    ? (
+                      transferState.errorMessage
+                        ? transferState.errorMessage
+                        : 'There was a problem transferring coins'
+                    )
+                    : transferState.inProgress
+                      ? (
+                        'Transferring Coins'
+                        <i className='fa fa-spinner fa-pulse' />
+                      )
+                      : transferState.complete
+                        ? (
+                          Coins Transferred
+                          <i className='fa fa-check' />
+                          <a
+                            target='_blank'
+                            className='small-link'
+                            href={`${etherscanAddress}/tx/${transferState.transactionHash}`}
+                          >
+                            etherscan
+                          </a>
+                        )
+                        : null
                 }
               </div>,
               <div  className='progress-row' key='1'>
-                Your service may or may not be available shortly at&nbsp;
-                <a
-                  target='_blank'
-                  className='small-link'
-                  href={serviceUrl}
-                >
-                  link
-                </a>
+                {
+                  deployState.error
+                    ? (
+                      deployState.errorMessage
+                        ? deployState.errorMessage
+                        : 'There was a problem deploying your service'
+                    )
+                    : deployState.inProgress
+                      ? (
+                        'Deploying service'
+                        <i className='fa fa-spinner fa-pulse' />
+                      )
+                      : deployState.complete
+                        ? (
+                          Service Deployed
+                          <i className='fa fa-check' />
+                          <a
+                            target='_blank'
+                            className='small-link'
+                            href={deployState.serviceUrl}
+                          >
+                            link
+                          </a>
+                        )
+                        : null
+                }
               </div>
             ]
-          } else if (state === 'transferring' || state === 0) {
-            return (
-              <div className='progress-row'>
-                Transferring Coins&nbsp;
-                <i className='fa fa-spinner fa-pulse' />
-              </div>
-            )
-          } else if (state === 'deploying' || state === 1) {
-            return [
-              <div className='progress-row' key='0'>
-                Coins Transferred&nbsp;
-                <i className='fa fa-check' />&nbsp;
-                <a
-                  target='_blank'
-                  className='small-link'
-                  href={`${etherscanAddress}/tx/${txHash}`}
-                >
-                  etherscan
-                </a>
-              </div>,
-              <div className='progress-row' key='1'>
-                Deploying Service&nbsp;
-                <i className='fa fa-spinner fa-pulse' />
-              </div>
-            ]
-          } else if (state === 'done' || state === 2) {
-            return [
-              <div className='progress-row' key='0'>
-                Coins Transferred&nbsp;
-                <i className='fa fa-check' />&nbsp;
-                <a
-                  target='_blank'
-                  className='small-link'
-                  href={`${etherscanAddress}/tx/${txHash}`}
-                >
-                  etherscan
-                </a>
-              </div>,
-              <div className='progress-row' key='1'>
-                Service Deployed&nbsp;
-                <i className='fa fa-check' />&nbsp;
-                <a
-                  target='_blank'
-                  className='small-link'
-                  href={serviceUrl}
-                >
-                  link
-                </a>
-              </div>
-            ]
-          } else {
-            return null
           }
-        })
-      }
-    </div>
+        }
+      </div>
+    )
+
+    // return <div className='progress column'>
+    //   {
+    //     Object.keys(this.props.tasks).map(serviceAddress => {
+    //       const {
+    //         state,
+    //         txHash,
+    //         serviceUrl,
+    //         errorMessage,
+    //       } = this.props.tasks[serviceAddress]
+    //
+    //       if (state === 'error' || state === 3) {
+    //         return [
+    //           <div className='progress-row' key='0'>
+    //             {
+    //               errorMessage
+    //                 ? errorMessage
+    //                 : 'There was a problem deploying the service.'
+    //             }
+    //           </div>,
+    //           <div  className='progress-row' key='1'>
+    //             Your service may or may not be available shortly at&nbsp;
+    //             <a
+    //               target='_blank'
+    //               className='small-link'
+    //               href={serviceUrl}
+    //             >
+    //               link
+    //             </a>
+    //           </div>
+    //         ]
+    //       } else if (state === 'transferring' || state === 0) {
+    //         return (
+    //           <div className='progress-row'>
+    //             Transferring Coins&nbsp;
+    //             <i className='fa fa-spinner fa-pulse' />
+    //           </div>
+    //         )
+    //       } else if (state === 'deploying' || state === 1) {
+    //         return [
+    //           <div className='progress-row' key='0'>
+    //             Coins Transferred&nbsp;
+    //             <i className='fa fa-check' />&nbsp;
+    //             <a
+    //               target='_blank'
+    //               className='small-link'
+    //               href={`${etherscanAddress}/tx/${txHash}`}
+    //             >
+    //               etherscan
+    //             </a>
+    //           </div>,
+    //           <div className='progress-row' key='1'>
+    //             Deploying Service&nbsp;
+    //             <i className='fa fa-spinner fa-pulse' />
+    //           </div>
+    //         ]
+    //       } else if (state === 'done' || state === 2) {
+    //         return [
+    //           <div className='progress-row' key='0'>
+    //             Coins Transferred&nbsp;
+    //             <i className='fa fa-check' />&nbsp;
+    //             <a
+    //               target='_blank'
+    //               className='small-link'
+    //               href={`${etherscanAddress}/tx/${txHash}`}
+    //             >
+    //               etherscan
+    //             </a>
+    //           </div>,
+    //           <div className='progress-row' key='1'>
+    //             Service Deployed&nbsp;
+    //             <i className='fa fa-check' />&nbsp;
+    //             <a
+    //               target='_blank'
+    //               className='small-link'
+    //               href={serviceUrl}
+    //             >
+    //               link
+    //             </a>
+    //           </div>
+    //         ]
+    //       } else {
+    //         return null
+    //       }
+    //     })
+    //   }
+    // </div>
   }
 }
 
